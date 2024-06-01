5760cb3ca0baa05b0890f14294dbaac4f243c0a4
IsaacAvila29 <149898781+IsaacAvila29@users.noreply.github.com>
2024-06-01 10:28:23 -0600

    Add files via upload

---
+++ CarController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public WheelCollider frontLeftWheel;
+    public WheelCollider frontRightWheel;
+    public WheelCollider rearLeftWheel;
+    public WheelCollider rearRightWheel;
+
+    public Transform frontLeftTransform;
+    public Transform frontRightTransform;
+    public Transform rearLeftTransform;
+    public Transform rearRightTransform;
+
+    public float maxMotorTorque = 500f;
+    public float maxSteeringAngle = 30f;
+
+    private void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        frontLeftWheel.steerAngle = steering;
+        frontRightWheel.steerAngle = steering;
+
+        rearLeftWheel.motorTorque = motor;
+        rearRightWheel.motorTorque = motor;
+
+        UpdateWheelPoses();
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontLeftWheel, frontLeftTransform);
+        UpdateWheelPose(frontRightWheel, frontRightTransform);
+        UpdateWheelPose(rearLeftWheel, rearLeftTransform);
+        UpdateWheelPose(rearRightWheel, rearRightTransform);
+    }
+
+    private void UpdateWheelPose(WheelCollider collider, Transform transform)
+    {
+        Vector3 pos;
+        Quaternion quat;
+        collider.GetWorldPose(out pos, out quat);
+        transform.position = pos;
+        transform.rotation = quat;
+    }
+}
